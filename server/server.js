80753fdafa88d146ab6e686c2e5fbaa3ca302c07
ChaseWNorton <chasenorton@me.com>
2018-03-06 18:27:19 -0800

    dified for new data entry

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const parser = require('body-parser');
 app.use(parser.json());
 app.use(express.static('../client'));
+app.use('/:id', express.static('../client'));
+
 app.listen(4040, function(err) {
   if (err) console.error(err);
   else console.log('Were live baby!');
